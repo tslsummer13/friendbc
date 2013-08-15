8f12f0ca53e1bb46fd7a17c19bd792bba4897c3a
Raghu Betina <rbetina@gmail.com>
2013-08-15 15:31:39 -0500

    Enhanced sign-in with helpful flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
 
     if @user.present? && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:notice] = 'Signed in successfully.'
       redirect_to root_url
     else
+      flash[:error] = 'Something went wrong. Please try again.'
       redirect_to new_session_url
     end
   end

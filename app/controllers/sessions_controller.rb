e5e28af9b89ee67c8d338e853632597d4220d046
Raghu Betina <rbetina@gmail.com>
2013-08-15 15:20:46 -0500

    Added core sign-in logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by username: params[:username]
+
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_url
+    else
+      redirect_to new_session_url
+    end
   end
 
   def destroy

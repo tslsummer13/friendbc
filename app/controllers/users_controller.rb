d6f09c6a27708f5635a1d43c947232ffb3efc26e
Raghu Betina <rbetina@gmail.com>
2013-08-15 22:06:46 -0500

    In users#create, pulled user parameters out into a standalone hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new({
+    user_params = {
       username: params[:username],
       first_name: params[:first_name],
       last_name: params[:last_name],
@@ -25,7 +25,9 @@ class UsersController < ApplicationController
       password_confirmation: params[:password_confirmation],
       facebook_access_token: params[:facebook_access_token],
       facebook_id: params[:facebook_id]
-    })
+    }
+
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to users_url

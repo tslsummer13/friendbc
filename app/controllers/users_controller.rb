ad7b6992e16f1ebcd41a51b0a35be8fb7f73fe87
Raghu Betina <rbetina@gmail.com>
2013-08-15 22:47:45 -0500

    Longhand way of declaring how to respond to users#index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html { render 'index' }
+    end
   end
 
   def show

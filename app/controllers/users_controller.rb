18196f6525dd61f6133d43e77567618cd9c833ef
Raghu Betina <rbetina@gmail.com>
2013-08-15 21:51:36 -0500

    Updated UsersController#create with mass assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.username = params[:username]
-    @user.first_name = params[:first_name]
-    @user.last_name = params[:last_name]
-    @user.password = params[:password]
-    @user.password_confirmation = params[:password_confirmation]
-    @user.facebook_access_token = params[:facebook_access_token]
-    @user.facebook_id = params[:facebook_id]
+    @user = User.new({
+      username: params[:username],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      facebook_access_token: params[:facebook_access_token],
+      facebook_id: params[:facebook_id]
+    })
 
     if @user.save
       redirect_to users_url

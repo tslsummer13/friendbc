e0f55bd10b59eaa776c63ec4bbe5924204baa321
Raghu Betina <rbetina@gmail.com>
2013-08-19 00:18:49 -0500

    Improved display_video helper to support Vimeo as a host

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
   def display_video(url)
     parsed_url = URI.parse(url)
-    if parsed_url.host.include?('youtube')
+    case
+    when parsed_url.host.include?('youtube')
       query_params = Rack::Utils.parse_query(parsed_url.query)
       "<iframe width=\"853\" height=\"505\" src=\"//www.youtube.com/embed/#{query_params['v']}\" frameborder=\"0\" allowfullscreen></iframe>".html_safe
+    when parsed_url.host.include?('vimeo')
+      "<iframe src=\"http://player.vimeo.com/video/#{url.split('/').last}?byline=0&amp;portrait=0&amp;badge=0\" width=\"853\" height=\"479\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>".html_safe
     else
       link_to "Click here to watch on #{parsed_url.host} <i class=\"icon-external-link\"></i>".html_safe, url, target: '_blank', class: 'btn btn-primary btn-large'
     end

00a49fd66b4740ad8c8dfccf1c65d4898845613f
Raghu Betina <rbetina@gmail.com>
2013-08-19 00:09:15 -0500

    Pulled video display logic out into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def display_video(url)
+    parsed_url = URI.parse(url)
+    if parsed_url.host.include?('youtube')
+      query_params = Rack::Utils.parse_query(parsed_url.query)
+      "<iframe width=\"853\" height=\"505\" src=\"//www.youtube.com/embed/#{query_params['v']}\" frameborder=\"0\" allowfullscreen></iframe>".html_safe
+    else
+      link_to "Click here to watch on #{parsed_url.host} <i class=\"icon-external-link\"></i>".html_safe, url, target: '_blank', class: 'btn btn-primary btn-large'
+    end
+  end
 end

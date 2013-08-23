674255b7d40ebef65d4765da801d3dfde65a5fb9
Raghu Betina <rbetina@gmail.com>
2013-08-23 08:57:42 -0500

    Completed OAuth flow

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,22 @@
+require 'open-uri'
+
 class AuthController < ApplicationController
   def facebook
     app_id = "149705581903855"
     app_secret = "9892ab887a2c08477ac8536c457a45e8"
     redirect_uri = "http://localhost:3000/auth/facebook"
+    code = params[:code]
 
-    # Write code here to exchange the code and our app secret for the access token
-    # Save the access token to the appropriate User row
-    # Redirect back to the user's show page
-    # https://developers.facebook.com/docs/facebook-login/login-flow-for-web-no-jssdk/#confirm
+    endpoint_url = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{code}"
+
+    result = open(endpoint_url).read
+    parsed_result = Rack::Utils.parse_query(result)
+
+    access_token = parsed_result["access_token"]
+
+    current_user.facebook_access_token = access_token
+    current_user.save
+
+    redirect_to user_url(current_user)
   end
 end
